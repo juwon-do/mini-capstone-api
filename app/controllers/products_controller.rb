555cdeaafbf05002fd9ead5cfbb838d457adae09
Juwon Do <nobeldr13@gmail.com>
2022-12-07 13:16:03 -0600

    add create

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,15 @@ class ProductsController < ApplicationController
     product = Product.find_by(id: params[:id])
     render json: product.as_json
   end
+  def create
+    product = Product.new(
+      id: "101",
+      name: "car",
+      price: "30000",
+      image_url: "https://tesla-cdn.thron.com/delivery/public/image/tesla/8c26f779-11e5-4cfc-bd7c-dcd03b18ff88/bvlatuR/std/4096x2561/Model-X-Main-Hero-Desktop-LHD",
+      description: "Electric SUV Car"
+    )
+    product.save
+    render json: product.as_json
+  end
 end
