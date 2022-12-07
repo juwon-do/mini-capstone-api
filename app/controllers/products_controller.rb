24292450e2595bd77488af80d8c58d50c361e851
Juwon Do <nobeldr13@gmail.com>
2022-12-07 11:49:33 -0600

    add url parameters

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,12 @@ class ProductsController < ApplicationController
     product = Product.all
     render json: product.as_json
   end
+  def show
+    
+    p "params"
+    p params["id"]
+    p "params"
+    products = Product.find_by(id: params["id"])
+    render json: products.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/get_product" => "products#all"
+
+  get "/ind_product/:id" => "products#show"
 end
