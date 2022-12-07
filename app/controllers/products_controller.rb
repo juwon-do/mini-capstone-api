1572bb916eb428e08291772f90a7f8ea41e98304
Juwon Do <nobeldr13@gmail.com>
2022-12-07 14:10:54 -0600

    add update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,4 +18,14 @@ class ProductsController < ApplicationController
     product.save
     render json: product.as_json
   end
+  def update
+    product = Product.find_by(id: params[:id])
+    product.id = params[:id]
+    product.name = params[:name]
+    product.price = params[:price]
+    product.image_url = params[:image_url]
+    product.description = params[:description]
+    product.save
+    render json: product.as_json
+  end
 end
