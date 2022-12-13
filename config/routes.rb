53fa3a45acf63ffcfd94cd2f2e57485705911066
Juwon Do <nobeldr13@gmail.com>
2022-12-13 12:22:36 -0600

    updating empty supplier_id data in product table

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   post "/products" => "products#create"
   patch "/products/:id" => "products#update"
   delete "/products/:id" => "products#delete"
+  get "/suppliers" => "products#index"
+  get "/suppliers/:id" => "products#show"
+  post "/suppliers" => "suppliers#create"
+  patch "/suppliers/:id" => "suppliers#update"
+  delete "/suppliers/:id" => "suppliers#delete"
 end
