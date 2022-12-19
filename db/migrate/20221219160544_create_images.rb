bf91e78b294b211e85390a1205e0ef569991a831
Juwon Do <nobeldr13@gmail.com>
2022-12-19 10:07:43 -0600

    drop and recreate image model

--- db/migrate/20221219160544_create_images.rb
+++ db/migrate/20221219160544_create_images.rb
@@ -2,7 +2,7 @@ class CreateImages < ActiveRecord::Migration[7.0]
   def change
     create_table :images do |t|
       t.string :url
-      t.string :product_id
+      t.integer :product_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_16_174821) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_19_160544) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,7 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_16_174821) do
 
   create_table "images", force: :cascade do |t|
     t.string "url"
-    t.string "product_id"
+    t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
